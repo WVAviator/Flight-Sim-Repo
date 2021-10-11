25251b64805f3b106b4e37bfe71a25693c2a75aa
WVAviator <wvaviator@gmail.com>
2021-10-11 17:25:32 -0500

    Added ScriptableObjects

---
+++ Assets/Scripts/ScriptableObjects/AerodynamicProperties.cs
@@ -0,0 +1,22 @@
+﻿
+
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Flight/Aerodynamic Profile")]
+public class AerodynamicProperties : ScriptableObject
+{
+    [SerializeField] float torque;
+    public float Torque => torque;
+
+    [SerializeField] float reverse;
+    public float Reverse => reverse;
+
+    [SerializeField] float thrust;
+    public float Thrust => thrust;
+    
+    [SerializeField] float speed;
+    public float Speed => speed;
+    
+    [SerializeField] float rotationSpeed;
+    public float RotationSpeed => rotationSpeed;
+}
