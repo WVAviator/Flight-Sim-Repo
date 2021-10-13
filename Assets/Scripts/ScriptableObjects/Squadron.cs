213000c026400c5811a60fcd4cd0405dad2fca8c
WVAviator <wvaviator@gmail.com>
2021-10-13 09:07:18 -0500

    Fixed prefabs and AI test scene

--- Assets/Scripts/ScriptableObjects/Squadron.cs
+++ Assets/Scripts/ScriptableObjects/Squadron.cs
@@ -6,7 +6,7 @@ namespace FlightSim
     [CreateAssetMenu(menuName = "Flight/Squadron")]
     public class Squadron : ScriptableObject
     {
-        [SerializeField] string name;
-        public string Name => name;
+        [SerializeField] string squadronName;
+        public string SquadronName => squadronName;
     }
 }
